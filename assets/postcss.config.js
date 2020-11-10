18d7755222bd0c30c66dc4aca5b7e5b391fd7406
MuhammadMullah <muhammadmullah16@gmail.com>
2020-11-11 01:28:50 +0300

    config tailwindcss

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -1,7 +1,19 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    '../**/*.html.eex',
+    '../**/*.html.leex',
+    '../**/views/**/*.ex',
+    './js/**/*.js'
+  ],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
 module.exports = {
   plugins: [
     require("postcss-import"),
-    require("tailwindcss")("./tailwind.config.js"),
+    require("tailwindcss"),
     require('autoprefixer'),
+    ...require(process.env.NODE_ENV === "production" ? [purgecss] : [])
+
   ],
  };
