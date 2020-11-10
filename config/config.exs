2e77d9cadd6435ada11c5fcc93fb88ad713ce9bf
MuhammadMullah <muhammadmullah16@gmail.com>
2020-11-10 05:54:32 +0300

    ready.... set... code.....!!!

---
+++ config/config.exs
@@ -0,0 +1,30 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :short_url,
+  ecto_repos: [ShortUrl.Repo]
+
+# Configures the endpoint
+config :short_url, ShortUrlWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "eh7MjqcBsWZz2RK8lWVJYAXeKLycUUljsUqy/8+5UEMMiEwS6RREqXowej974o6g",
+  render_errors: [view: ShortUrlWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: ShortUrl.PubSub, adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
---
+++ lib/short_url/application.ex
@@ -0,0 +1,31 @@
+defmodule ShortUrl.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Start the Ecto repository
+      ShortUrl.Repo,
+      # Start the endpoint when the application starts
+      ShortUrlWeb.Endpoint
+      # Starts a worker by calling: ShortUrl.Worker.start_link(arg)
+      # {ShortUrl.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: ShortUrl.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    ShortUrlWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
